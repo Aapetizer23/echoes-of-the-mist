9dd0f75f58a81b98076c04389e8003d51a15f451
Aapetizer23 <matteomatttaw@gmail.com>
2026-07-01 10:36:21 +0900

    Point and Click Second Prototype

---
+++ Matteo/Assets/Scripts/scene_5.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_button_button_down() -> void:
+	get_tree().change_scene_to_file("res://Matteo/Assets/Scenes/Scene 5A.tscn")
+
+
+func _on_button_3_button_down() -> void:
+	get_tree().change_scene_to_file("res://Matteo/Assets/Scenes/Scene 5B.tscn")
+
+
+func _on_button_2_button_down() -> void:
+	get_tree().change_scene_to_file("res://Matteo/Assets/Scenes/Scene 6.tscn")
+
+
+func _on_button_4_button_down() -> void:
+	get_tree().change_scene_to_file("res://Matteo/Assets/Scenes/Scene 4.tscn")
---
+++ Matteo/Assets/Scripts/torch.gd
@@ -0,0 +1,17 @@
+extends Button
+
+@export var torch_ID: int
+var is_on
+
+func _ready():
+	is_on = global_torch_manager.torch_array[torch_ID]
+	$Off.visible = !is_on
+	$On.visible = is_on
+	
+
+
+func _on_button_down() -> void:
+	if !is_on:
+		$Off.visible = false
+		$On.visible = true
+		global_torch_manager.torch_lit(torch_ID)
